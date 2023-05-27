ce9e9afc084302133208e8e573bba18741ced039
dcrisanto <dorelly.crisanto@gmail.com>
2023-05-27 00:29:10 -0500

    creación de ruta dashboard, creación de componente Header, Layout, páginas Home, Login y dashboard

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,11 @@
+import DashboardPage from '@pages/DashboardPage';
+
+const Dashboard = () => {
+  return (
+    <>
+      <DashboardPage />
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ src/layout/Layout.jsx
@@ -0,0 +1,11 @@
+import Header from '@components/Header';
+import Nav from '@common/Nav';
+
+export default function Layout() {
+    return(
+        <>
+            <Header></Header>
+            <Nav></Nav>
+        </>
+    );
+};
