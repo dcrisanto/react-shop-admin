ff6cac4d0316535536ed6a3d4f7dec4f549aae87
dcrisanto <dorelly.crisanto@gmail.com>
2023-05-31 14:39:57 -0500

    guardando el token en Cookie y consumiendo la api profile

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,10 +1,6 @@
-'use client';
-import { useAuth } from '@hooks/useAuth';
 import LoginPage from '@pages/LoginPage';
 
 const Login = () => {
-  const auth = useAuth();
-  console.log(auth);
   return (
     <>
       <LoginPage />
