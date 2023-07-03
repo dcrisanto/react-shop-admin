2c8739dc7ab72d06eedf52d76136c30e393f5676
dcrisanto <dorelly.crisanto@gmail.com>
2023-07-03 18:45:20 -0500

    .. agregando width y heigth en etiqueta Image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['tailwindui.com', 'picsum.photos', 'placeimg.com', 'estaticos-cdn.prensaiberica.es']
+    domains: ['tailwindui.com', 'picsum.photos', 'placeimg.com', 'estaticos-cdn.prensaiberica.es', 'ui-avatars.com', 'api.escuelajs.co', 'images.pexels.com', 'source.unsplash.com']
   }
 }
 
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -113,7 +113,7 @@ const ProductsPage = () => {
                       <td className="px-6 py-4 whitespace-nowrap">
                         <div className="flex items-center">
                           <div className="flex-shrink-0 h-10 w-10">
-                            <Image className="h-10 w-10 rounded-full" src={product.images[0]} alt="" />
+                            <img className="h-10 w-10 rounded-full" src={product.images[0]} alt="" width="50" height="50" />
                           </div>
                           <div className="ml-4">
                             <div className="text-sm font-medium text-gray-900">{product.title}</div>
